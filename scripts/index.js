89532bac7c7475e760523dfd908865807a72becb
LLG <88912958+LLG-BUAA@users.noreply.github.com>
2023-04-10 10:59:10 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 9, 2023',
-  name: 'Tang'
+  birthdate: 'Apr 13, 2023',
+  name: 'Darlene'
 };
 
 function hideEverything() {
