c79327d3d1d22ad08a04f512dc87bed999c202ce
LLG <88912958+LLG-BUAA@users.noreply.github.com>
2023-04-10 09:58:00 +0800

    test

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jun 10, 2023',
+  birthdate: 'Apr 10, 2023',
   name: '💫'
 };
 
